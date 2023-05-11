7e72295476fe7e9661b3ddfc5bf4123721412e5d
youngmurph <conormurphy@gmx.de>
2023-05-10 22:50:03 -0400

    Add functionality for game

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the readme for my Rock, Paper, Scissors console game as part of The Odin Project.
+
+The game will prompt the user to make their choice, then generate a random computer choice, before returning a message to the user indicating whether they have won or lost.
---
+++ script.js
@@ -0,0 +1,44 @@
+var choices = ['Rock', 'Paper', 'Scissors'];
+
+function getComputerChoice() {
+     return choices[Math.floor(Math.random() * choices.length)];  
+} 
+
+const win = "You won!";
+const tie = "You tied!";
+const loss = "You lose!";
+
+function firstRound(playerSelection, computerSelection) {
+    
+    getComputerChoice();
+
+    var computerSelection = getComputerChoice();
+    var playerSelection = prompt("Rock, paper or scissors?");
+
+    console.log("Computer: ", computerSelection);
+    console.log("Player: ", playerSelection);
+
+    if (playerSelection === computerSelection){
+        console.log(tie);
+    } else if (playerSelection === "Rock".toLowerCase() && computerSelection === "Scissors" ||
+                playerSelection === "Scissors".toLowerCase() && computerSelection === "Paper" ||
+                playerSelection === "Paper".toLowerCase() && computerSelection === "Rock"){
+        console.log(win);
+                } else if (playerSelection === null || playerSelection === ""){
+        console.log("Invalid input, try again");
+                } else {
+                    console.log(loss);
+                };
+
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        firstRound();
+    }
+}
+
+game();
+
+
+
