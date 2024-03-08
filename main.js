03a7146357f7c8b53b69c31a39dc15af5c6e2349
Conor Murphy <conormurphy@gmx.de>
2024-03-07 22:33:36 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,36 @@
+/* Choose a string from three possible choices. */
+/* Print the chosen string to the console. */
+
+let choice = Math.floor(Math.random() * 3);
+
+function getComputerChoice(choice) {
+
+    if (choice === 1) {
+        return "rock";
+    } else if (choice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+} 
+
+/* Prompt user for their choice with no case sensitivity */
+/* Run getComputerChoice()*/
+
+let playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+let computerSelection = getComputerChoice();
+
+
+function firstRound (playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection === "scissors") {
+        return "You win, congratulations!";
+    } else if (playerSelection == "paper" && computerSelection === "rock") {
+        return "You win, congratulations!"; 
+    } else if (playerSelection == "scissors" && computerSelection === "paper") {
+        return "You win, congratulations!";
+    } else {
+        return "You lose!";
+    }
+}
+
+console.log(firstRound());
