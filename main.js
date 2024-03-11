ffb1a61ee146ce77394001689ed75a0e8948cf0b
Conor Murphy <conormurphy@gmx.de>
2024-03-11 19:23:29 -0400

    Finish first round functionality.

--- main.js
+++ main.js
@@ -2,35 +2,41 @@
 /* Print the chosen string to the console. */
 
 function getComputerChoice() {
-    let choice = Math.floor((Math.random() * 3) + 1);
-    return choice;
+    let choices = (['Rock', 'Paper', 'Scissors']);
+    let result = choices[Math.floor((Math.random() * 3))];
+    return result;
 } 
+
+/* console.log(getComputerChoice()); */
 
 /* getComputerChoice(); */
 
 /* Prompt user for their choice with no case sensitivity */
 /* Run getComputerChoice()*/
 
-let playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
-let computerSelection = getComputerChoice();
-
-
 function firstRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === 3) {
-        alert("You win, rock beats scissors!");
-    } else if (playerSelection = "paper" && computerSelection === 1) {
-        alert("You win, paper beats rock!"); 
-    } else if (playerSelection === "scissors" && computerSelection === 2) {
-        alert("You win, scissors beats paper!");
-    } else if (playerSelection === "rock" && computerSelection === 1) {
-        alert("It's a draw!");
-    } else if (playerSelection === "paper" && computerSelection === 2) {
-        alert("It's a draw!");
-    } else if (playerSelection === "scissors" && computerSelection === 3) {
-        alert("It's a draw!");
-    } else {
-        alert("You lose!")
-    }
-}
 
-firstRound(playerSelection, computerSelection); 
+    let result;
+
+    if (playerSelection == 'rock' && computerSelection === 'Scissors') {
+        result = "You win, rock beats scissors!";
+    } else if (playerSelection == 'paper' && computerSelection === 'Rock') {
+        result = "You win, paper beats rock!"; 
+    } else if (playerSelection == 'scissors' && computerSelection === 'Paper') {
+        result = "You win, scissors beats paper!";
+    } else if (playerSelection == 'rock' && computerSelection === 'Rock') {
+        result = "It's a draw!";
+    } else if (playerSelection == 'paper' && computerSelection === 'Paper') {
+        result = "It's a draw!";
+    } else if (playerSelection == 'scissors' && computerSelection === 'Scissors') {
+        result = "It's a draw!";
+    } else {
+        result = "You lose!";
+    }
+
+    return result;
+} 
+
+const playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+const computerSelection = getComputerChoice();
+
