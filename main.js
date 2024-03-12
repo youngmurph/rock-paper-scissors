f76f7cc26503fcdc30fed08a8fd48a9c12d012da
Conor Murphy <conormurphy@gmx.de>
2024-03-11 20:57:34 -0400

    Finish game functionality.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /* Choose a string from three possible choices. */
 /* Print the chosen string to the console. */
 
-function getComputerChoice() {
+/* function getComputerChoice() {
     let choices = (['Rock', 'Paper', 'Scissors']);
     let result = choices[Math.floor((Math.random() * 3))];
     return result;
-} 
+} */
 
 /* console.log(getComputerChoice()); */
 
@@ -37,6 +37,67 @@ function firstRound(playerSelection, computerSelection) {
     return result;
 } 
 
-const playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
-const computerSelection = getComputerChoice();
+/* function playerSelection() { 
+    prompt("Rock, paper, or scissors?").toLowerCase();
+}
+const computerSelection = getComputerChoice(); */
 
+function playGame() {
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    function computerSelection() {
+        let choices = (['Rock', 'Paper', 'Scissors']);
+        let result = choices[Math.floor((Math.random() * 3))];
+        return result;
+    }
+    function playerSelection() {
+        let choice = prompt("Rock, paper, or scissors?").toLowerCase();
+        return choice;
+    }
+
+    function playRound() {
+        let computer = computerSelection();
+        let player = playerSelection();
+
+        if (player == 'rock' && computer == 'Scissors') {
+            playerScore+= 1;
+        } else if (player == 'paper' && computer == 'Rock') {
+            playerScore+= 1;
+        } else if (player == 'scissors' && computer == 'Paper') {
+            playerScore+= 1;
+        } else if (player == 'rock' && computer == 'Rock') {
+            playerScore+= 0;
+            computerScore+= 0;
+        } else if (player == 'scissors' && computer == 'Scissors') {
+            playerScore+= 0;
+            computerScore+= 0;
+        } else if (player == 'paper' && computer == 'Paper') {
+            playerScore+= 0;
+            computerScore+= 0;
+        } else {
+            computerScore+= 1;
+        }
+
+        console.log(playerScore + ' - ' + computerScore);
+
+        }
+    
+    
+
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+
+    if (playerScore > computerScore) {
+        console.log("Congratulations, you win!");
+    } else {
+        console.log("You lose, better luck next time!");
+    }
+
+    }
+
+    playGame();
