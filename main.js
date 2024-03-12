cc32f54ca9165b85393d4b3a46785a08a65aefd2
Conor Murphy <conormurphy@gmx.de>
2024-03-11 21:44:29 -0400

    Clean up and remove redundant code

--- main.js
+++ main.js
@@ -1,47 +1,3 @@
-/* Choose a string from three possible choices. */
-/* Print the chosen string to the console. */
-
-/* function getComputerChoice() {
-    let choices = (['Rock', 'Paper', 'Scissors']);
-    let result = choices[Math.floor((Math.random() * 3))];
-    return result;
-} */
-
-/* console.log(getComputerChoice()); */
-
-/* getComputerChoice(); */
-
-/* Prompt user for their choice with no case sensitivity */
-/* Run getComputerChoice()*/
-
-function firstRound(playerSelection, computerSelection) {
-
-    let result;
-
-    if (playerSelection == 'rock' && computerSelection === 'Scissors') {
-        result = "You win, rock beats scissors!";
-    } else if (playerSelection == 'paper' && computerSelection === 'Rock') {
-        result = "You win, paper beats rock!"; 
-    } else if (playerSelection == 'scissors' && computerSelection === 'Paper') {
-        result = "You win, scissors beats paper!";
-    } else if (playerSelection == 'rock' && computerSelection === 'Rock') {
-        result = "It's a draw!";
-    } else if (playerSelection == 'paper' && computerSelection === 'Paper') {
-        result = "It's a draw!";
-    } else if (playerSelection == 'scissors' && computerSelection === 'Scissors') {
-        result = "It's a draw!";
-    } else {
-        result = "You lose!";
-    }
-
-    return result;
-} 
-
-/* function playerSelection() { 
-    prompt("Rock, paper, or scissors?").toLowerCase();
-}
-const computerSelection = getComputerChoice(); */
-
 function playGame() {
 
     let playerScore = 0;
@@ -84,8 +40,6 @@ function playGame() {
 
         }
     
-    
-
     playRound();
     playRound();
     playRound();
