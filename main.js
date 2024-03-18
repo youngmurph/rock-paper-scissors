9f3c47587ebd7d9828960173313d5520c294c8e0
Conor Murphy <conormurphy@gmx.de>
2024-03-18 19:18:53 -0400

    Update functionality with event listeners

--- main.js
+++ main.js
@@ -18,30 +18,90 @@ function firstRound(playerSelection, computerSelection) {
         result = "You lose!";
     }
 
-    return result;
+     return result;
 } 
 
-function playGame() {
-
-    let playerScore = 0;
-    let computerScore = 0;
-
     function computerSelection() {
-        let choices = (['Rock', 'Paper', 'Scissors']);
-        let result = choices[Math.floor((Math.random() * 3))];
-        return result;
-    }
+    let choices = (['Rock', 'Paper', 'Scissors']);
+    let result = choices[Math.floor((Math.random() * 3))];
+    return result;
+}; 
 
-    const chooseRock = document.getElementById("rock");
-    const choosePaper = document.getElementById("paper");
-    const chooseScissors = document.getElementById("scissors");
     
     function playerSelection() {
-        let choice = prompt("Rock, paper, or scissors?").toLowerCase();
-        return choice;
-    }
 
-    function playRound() {
+    let rock = document.querySelector("#rock");
+    let paper = document.querySelector("#paper");
+    let scissors = document.querySelector("#scissors");
+    let playerChoice;
+
+    rock.addEventListener('click', function(event) {
+        playerChoice = 'rock';
+    });
+
+    paper.addEventListener('click', function(event) {
+        playerChoice = 'paper';
+    });
+
+    scissors.addEventListener('click', function(event) {
+        playerChoice = 'scissors';
+    });
+
+    return playerChoice;
+};
+
+/*  function playGame() { */
+
+
+
+    
+   /* function playerSelection() {
+    
+        paper.onclick = function() {
+            choice = 'paper';
+        }
+    
+        scissors.onclick = function() {
+            choice = 'scissors';
+        }
+
+        return choice;
+
+    }; */
+
+    let computer = computerSelection();
+    let player = playerSelection(); 
+
+       /*  let playerScore = 0;
+        let computerScore = 0;
+
+        if (player == 'rock' && computer == 'Scissors') {
+            playerScore+= 1;
+        } else if (player == 'paper' && computer == 'Rock') {
+            playerScore+= 1;
+        } else if (player == 'scissors' && computer == 'Paper') {
+            playerScore+= 1;
+        } else if (player == 'rock' && computer == 'Rock') {
+            playerScore+= 0;
+            computerScore+= 0;
+        } else if (player == 'scissors' && computer == 'Scissors') {
+            playerScore+= 0;
+            computerScore+= 0;
+        } else if (player == 'paper' && computer == 'Paper') {
+            playerScore+= 0;
+            computerScore+= 0;
+        } else {
+            computerScore+= 1;
+        }
+
+        if (playerScore > computerScore) {
+            console.log("Congratulations, you win!");
+        } else {
+            console.log("You lose, better luck next time!");
+        }
+};
+
+   /* function playRound() {
         let computer = computerSelection();
         let player = playerSelection();
 
@@ -68,14 +128,14 @@ function playGame() {
 
         }
     
-    
+    playRound(); */
 
-    if (playerScore > computerScore) {
+  /* if (playerScore > computerScore) {
         console.log("Congratulations, you win!");
     } else {
         console.log("You lose, better luck next time!");
-    }
+    } */
 
-    }
 
-    playGame();
+
+    /* playGame(); */
